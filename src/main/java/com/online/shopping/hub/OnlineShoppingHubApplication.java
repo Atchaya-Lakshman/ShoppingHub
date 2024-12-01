b3eba571ca5ac115a976faacc16ac108f0b57094
Atchaya-Lakshman <lakshmananyanavee@gmail.com>
2024-12-01 14:37:46 +0900

    Commiting the Framework

---
+++ src/main/java/com/online/shopping/hub/OnlineShoppingHubApplication.java
@@ -0,0 +1,12 @@
+package com.online.shopping.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingHubApplication.class, args);
+	}
+}
---
+++ src/test/java/com/online/shopping/hub/ECommerceProjectForOnlineShoppingHubApplicationTests.java
@@ -0,0 +1,15 @@
+package com.online.shopping.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+
+@ContextConfiguration(classes = OnlineShoppingHubApplication.class)
+class ECommerceProjectForOnlineShoppingHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
